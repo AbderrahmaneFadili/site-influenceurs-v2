be4f0181b14cf1e24da1f5d5ef5ce1761be87f66
AbderrahmaneFadili <abdouelrfadili@gmail.com>
2022-03-07 10:14:07 +0000

    Set up the server

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+//CORS options
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+//Set CORS Options
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple request
+app.get("/", (req, res) => {
+  res.json({
+    message: "Bienvenue sur l'API REST Site Influenceurs V2.",
+  });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
