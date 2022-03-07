e387f39d0b6bf809b169c0526203700b635a3402
AbderrahmaneFadili <abdouelrfadili@gmail.com>
2022-03-07 10:01:28 +0000

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Site Influenceurs V2 : Client</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
