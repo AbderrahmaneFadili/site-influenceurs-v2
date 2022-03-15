37ad4f7dbdce82e09c3f9503767d0499b38fb9c2
AbderrahmaneFadili <abdouelrfadili@gmail.com>
2022-03-15 15:37:34 +0000

    Admin Template Set up in the client/React

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
   render() {
     return (
-      <div>
-        <h1>Site Influenceurs V2 : Client</h1>
+      <div className="container">
+        <h1>Count : {this.state.count}</h1>
+        <button
+          className="btn btn-success"
+          onClick={() =>
+            this.setState({
+              count: this.state.count + 1,
+            })
+          }
+        ></button>
       </div>
     );
   }
