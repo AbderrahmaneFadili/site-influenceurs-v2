f7c32911fb8c728f2034d45e28d8487841c09bc6
AbderrahmaneFadili <abdouelrfadili@gmail.com>
2022-03-16 16:22:29 +0000

    Create Register UI in /client/

--- client/src/components/Manager/Register/ManagerRegister.js
+++ client/src/components/Manager/Register/ManagerRegister.js
@@ -1,7 +1,88 @@
 import React from "react";
 
 const ManagerRegister = () => {
-  return <div>ManagerRegister</div>;
+  return (
+    <>
+      <div className="register-page">
+        <div className="register-box">
+          <div className="register-logo">
+            <a href="/">
+              Site <b>Influenceur</b> V2
+            </a>
+          </div>
+          <div className="card">
+            <div className="card-body register-card-body">
+              <p className="login-box-msg">Inscrivez-vous</p>
+              <form method="post" className="mb-3">
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="nom et prénom"
+                  />
+                  <div className="input-group-append">
+                    <div className="input-group-text">
+                      <span className="fas fa-user" />
+                    </div>
+                  </div>
+                </div>
+                <div className="input-group mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="E-mail"
+                  />
+                  <div className="input-group-append">
+                    <div className="input-group-text">
+                      <span className="fas fa-envelope" />
+                    </div>
+                  </div>
+                </div>
+                <div className="input-group mb-3">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Mot de passe"
+                  />
+                  <div className="input-group-append">
+                    <div className="input-group-text">
+                      <span className="fas fa-lock" />
+                    </div>
+                  </div>
+                </div>
+                <div className="input-group mb-3">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Confirmez le mot de passe"
+                  />
+                  <div className="input-group-append">
+                    <div className="input-group-text">
+                      <span className="fas fa-lock" />
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  {/* /.col */}
+                  <div className="col">
+                    <button type="submit" className="btn btn-primary btn-block">
+                      Inscrire
+                    </button>
+                  </div>
+                  {/* /.col */}
+                </div>
+              </form>
+              <a href="/" className="text-center">
+                J'ai déjà un compte
+              </a>
+            </div>
+            {/* /.form-box */}
+          </div>
+          {/* /.card */}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ManagerRegister;
