b69cd12082d3e8db32e4000b53634ac91304d1f1
AbderrahmaneFadili <abdouelrfadili@gmail.com>
2022-03-17 14:24:55 +0000

    Add logout link in Sidebar => Dashboard Manager

--- client/src/components/Layouts/Dashboard/Dashboard.js
+++ client/src/components/Layouts/Dashboard/Dashboard.js
@@ -1,37 +1,14 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import SideBar from "../SideBar/SideBar";
-import { useRouteMatch } from "react-router-dom";
 import Content from "../Content/Content";
 
 
 
 
-const Dashboard = ({ links, dashboardTitle }) => {
-  let { path } = useRouteMatch();
+const Dashboard = ({ links, dashboardTitle, pages }) => {
 
-  const pages = [
-    {
-      path: path + "/campagnes",
-      page: () => <h1>Campagnes</h1>
-    },
-    {
-      path: path + "/studylevels",
-      page: () => <h1>Niveaux d\'étude</h1>
-    },
-    {
-      path: path + "/languages",
-      page: () => <h1>Langues</h1>
-    },
-    {
-      path: path + "/clients",
-      page: () => <h1>Clients</h1>
-    },
-    {
-      path: path + "/interests",
-      page: () => <h1>Centres d\'intérêt</h1>
-    },
-  ];
+
   return (
     <>
       {/* Navbar */}
--- client/src/components/Manager/Dashboard/ManagerDashboard.js
+++ client/src/components/Manager/Dashboard/ManagerDashboard.js
@@ -4,8 +4,9 @@ import Dashboard from "../../Layouts/Dashboard/Dashboard";
 
 const ManagerDashboard = () => {
 
-  const { url } = useRouteMatch();
+  const { url, path } = useRouteMatch();
 
+  //links 
   const linksList = [
     {
       to: url + "/campagnes",
@@ -31,12 +32,42 @@ const ManagerDashboard = () => {
       to: url + "/interests",
       title: 'Centres d\'intérêt',
       iconClassName: "fas fa-heart nav-icon",
+    }, {
+      to: "/",
+      title: "Se déconnecter",
+      iconClassName: "fas fa-sign-out-alt nav-icon"
+    }
+
+  ];
+
+  //pages
+  const pages = [
+    {
+      path: path + "/campagnes",
+      page: () => <h1>Campagnes</h1>
     },
+    {
+      path: path + "/studylevels",
+      page: () => <h1>Niveaux d\'étude</h1>
+    },
+    {
+      path: path + "/languages",
+      page: () => <h1>Langues</h1>
+    },
+    {
+      path: path + "/clients",
+      page: () => <h1>Clients</h1>
+    },
+    {
+      path: path + "/interests",
+      page: () => <h1>Centres d\'intérêt</h1>
+    },
+
   ];
 
 
   return (
-    <Dashboard links={linksList} dashboardTitle={"Dashboard Administrateur"} />
+    <Dashboard pages={pages} links={linksList} dashboardTitle={"Dashboard Administrateur"} />
   );
 };
 
